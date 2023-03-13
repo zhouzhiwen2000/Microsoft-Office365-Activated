509ba55164068873f8824ee996bda5dcc4fc9179
zhouzhiwen2000 <zhouzhiwen2000@gmail.com>
2023-03-13 19:24:55 +0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -7,11 +7,14 @@ cd /d %ProgramFiles(x86)%\Microsoft Office\Office16
 cd /d %ProgramFiles%\Microsoft Office\Office16
 for /f %x in ('dir /b ..\root\Licenses16\proplusvl_kms*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%x"
 pause
+for /f %x in ('dir /b ..\root\Licenses16\VisioProVL_KMS*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%x"
+pause
 cscript ospp.vbs /inpkey:XQNVK-8JYDB-WJ9W3-YJ8YR-WFG99
+cscript ospp.vbs /inpkey:PD3PC-RHNGV-FXJ29-8JK7D-RJRJK
 cscript ospp.vbs /unpkey:BTDRB >nul
 cscript ospp.vbs /unpkey:KHGM9 >nul
 cscript ospp.vbs /unpkey:CPQVG >nul
-cscript ospp.vbs /sethst:s8.uk.to
+cscript ospp.vbs /sethst:192.168.3.1
 cscript ospp.vbs /setprt:1688
 cscript ospp.vbs /act
 echo Product activation script executed.
